deba197d4a34905df511a60745bb6fb082a57e3a
Yuliya <yuliya.01707@gmail.com>
2024-10-23 22:04:19 +0200

    add test to validate login form with empty data

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -1,4 +1,19 @@
 package gov.login.secure;
 
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class LoginTest {
+    @Test
+    public void test1() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://secure.login.gov/");
+        String buttonSigninXpath = "//*[@id=\"new_user\"]/lg-submit-button/button";
+        By buttonSigninBy = By.xpath(buttonSigninXpath);
+        WebElement buttonSigninWebElement = driver.findElement(buttonSigninBy);
+        buttonSigninWebElement.click();
+    }
 }
