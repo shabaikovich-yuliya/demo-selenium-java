0e13a5f9048a57e7a6f4fe5f531eea460f8b4313
Yuliya <yuliya.01707@gmail.com>
2024-10-23 22:11:52 +0200

    add test to validate login form with any password

--- src/test/java/gov/login/secure/LoginTest.java
+++ src/test/java/gov/login/secure/LoginTest.java
@@ -31,4 +31,19 @@ public class LoginTest {
         WebElement buttonSigninWebElement = driver.findElement(buttonSigninBy);
         buttonSigninWebElement.click();
     }
+
+    @Test
+    public void test3() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://secure.login.gov/");
+        String inputPasswordXpath = "//*[@id=\"password-toggle-input-a0f50422\"]";
+        By inputParrwordBy = By.xpath(inputPasswordXpath);
+        WebElement inputPasswordWebElement = driver.findElement(inputParrwordBy);
+        inputPasswordWebElement.sendKeys("1q2w#E4r");
+
+        String buttonSigninXpath = "//*[@id=\"new_user\"]/lg-submit-button/button";
+        By buttonSigninBy = By.xpath(buttonSigninXpath);
+        WebElement buttonSigninWebElement = driver.findElement(buttonSigninBy);
+        buttonSigninWebElement.click();
+    }
 }
