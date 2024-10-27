d77064cc5020bc3b2c84365f3b4835e3934084dc
Yuliya <yuliya.01707@gmail.com>
2024-10-27 19:40:23 +0100

    add LoginMessage file to store messages

---
+++ src/test/java/LoginMessage.java
@@ -0,0 +1,6 @@
+public class LoginMessage {
+    public static final String INVALID_PASSWORD = "Your password must contain between 4 and 60 characters.";
+    public static final String INVALID_LOGIN = "Please enter a valid email or phone number.";
+    public static final String UNREGISTERED_USER = "Incorrect password for gfdgfd@gdsgfd.fg\n" +
+            "You can use a sign-in code, reset your password or try again.";
+}
