0c2eb609552244ca85edb985bc8b47d16d646f27
Yuliya <yuliya.01707@gmail.com>
2024-10-27 19:08:26 +0100

    add LoginXpath file to store xpathes

---
+++ src/test/java/LoginXpath.java
@@ -0,0 +1,5 @@
+public class LoginXpath {
+    public static final String INPUT_USER_LOGIN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
+    public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
+    public static final String BUTTON_SIGNIN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
+}
