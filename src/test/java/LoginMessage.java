4dc5bf290c5e31d8cebcd55f550468866c52f074
Yuliya <yuliya.01707@gmail.com>
2024-10-27 21:26:30 +0100

    refactor LoginXpath

--- src/test/java/LoginMessage.java
+++ src/test/java/LoginMessage.java
@@ -1,6 +1,5 @@
 public class LoginMessage {
     public static final String INVALID_PASSWORD = "Your password must contain between 4 and 60 characters.";
     public static final String INVALID_LOGIN = "Please enter a valid email or phone number.";
-    public static final String UNREGISTERED_USER = "Incorrect password for gfdgfd@gdsgfd.fg\n" +
-            "You can use a sign-in code, reset your password or try again.";
+    public static final String UNREGISTERED_USER = "Incorrect password for ";
 }
