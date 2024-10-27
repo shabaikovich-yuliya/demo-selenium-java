8ad654259928859a0af340f5078cd8c7237ba271
Yuliya <yuliya.01707@gmail.com>
2024-10-27 20:37:04 +0100

    add xpathes for error messages

--- src/test/java/LoginXpath.java
+++ src/test/java/LoginXpath.java
@@ -2,4 +2,7 @@ public class LoginXpath {
     public static final String INPUT_USER_LOGIN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[1]/input";
     public static final String INPUT_PASSWORD_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[1]/input";
     public static final String BUTTON_SIGNIN_XPATH = "/html/body/div[1]/div/div/div[2]/div/form/button[1]";
+    public static final String ERROR_MESSAGE_INVALID_PASSWORD = "/html/body/div[1]/div/div/div[2]/div/form/div[2]/div/div[2]";
+    public static final String ERROR_MESSAGE_INVALID_LOGIN = "/html/body/div[1]/div/div/div[2]/div/form/div[1]/div/div[2]";
+    public static final String ERROR_MESSAGE_UNREGISTERED_USER = "/html/body/div[1]/div/div/div[2]/div/header/div";
 }
