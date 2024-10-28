4b56dfeb171d04de1faacf5a6d315ffffab3cdc4
Yuliya <yuliya.01707@gmail.com>
2024-10-28 19:41:12 +0100

    add emptyPhone test

---
+++ src/test/java/by/mall/LoginTest.java
@@ -0,0 +1,25 @@
+package by.mall;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+    @Test
+    public void emptyPhone() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://emall.by/login/password");
+        LoginPage loginPage = new LoginPage(driver);
+
+        loginPage.sendKeysInputPassword("Password123!");
+        loginPage.clickButtonSignin();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+
+        Assertions.assertEquals(LoginMessages.EMPTY_PHONE, loginPage.getErrorMessage());
+
+    }
+
+}
