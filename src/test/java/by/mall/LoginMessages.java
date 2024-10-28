d43e94b2284358dc2b3e34199c2869e643bb18c6
Yuliya <yuliya.01707@gmail.com>
2024-10-28 19:20:26 +0100

    add one more text for error message to LoginMessages file

--- src/test/java/by/mall/LoginMessages.java
+++ src/test/java/by/mall/LoginMessages.java
@@ -3,5 +3,7 @@ package by.mall;
 public class LoginMessages {
     public static final String EMPTY_PHONE = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
     public static final String EMPTY_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
+    public static final String EMPTY_PHONE_AND_PASSWORD = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
+            "Поле \"Пароль\" обязательно для заполнения.";
     public static final String UNREGISTERED_USER = "Неверный логин или пароль";
 }
