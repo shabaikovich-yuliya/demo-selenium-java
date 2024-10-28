cdf4ae70e7fe2e81b0386ae2b5e5ae4ae5417195
Yuliya <yuliya.01707@gmail.com>
2024-10-28 19:40:49 +0100

    refactor LoginMessages

--- src/test/java/by/mall/LoginMessages.java
+++ src/test/java/by/mall/LoginMessages.java
@@ -1,7 +1,7 @@
 package by.mall;
 
 public class LoginMessages {
-    public static final String EMPTY_PHONE = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)";
+    public static final String EMPTY_PHONE = "Поле \"Номер телефона\" обязательно для заполнения.";
     public static final String EMPTY_PASSWORD = "Поле \"Пароль\" обязательно для заполнения.";
     public static final String EMPTY_PHONE_AND_PASSWORD = "Значение поля \"Номер телефона\" должно начинаться с +375 затем код (25|29|33|44) и далее 7 цифр (первая из которых не 0)\n" +
             "Поле \"Пароль\" обязательно для заполнения.";
